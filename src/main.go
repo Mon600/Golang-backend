4e8fdca6247d4020b239ee689b27196cac431af9
Monkster <miki.rulll2012@gmail.com>
2025-12-30 08:55:54 +0300

    develop add main.c

---
+++ src/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, world!"))
+	})
+	_ = http.ListenAndServe("localhost:5000", mux)
+}
